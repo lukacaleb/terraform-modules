fbe85421d89308c469df145ed47e65268017ad25
lukacaleb <calebluka@gmail.com>
2023-12-07 17:13:44 +0100

    first commit

---
+++ practice-website/backend.tf
@@ -0,0 +1,9 @@
+# store the terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket    = "backcontrol-files"
+    key       = "practice-website"
+    region    = "us-east-1"
+    profile   = "miniproject_accessKeys"
+  }
+}
